d6305789a1e24e5d7de8481c9e00c3a91159998a
copgADSI <48689287+copgADSI@users.noreply.github.com>
2023-03-30 16:21:21 -0500

    avances

--- resources/views/user/files/index.blade.php
+++ resources/views/user/files/index.blade.php
@@ -9,23 +9,33 @@
 
                     <div class="card-body">
                         <div class="row">
-                            <div class="col md-4 card m-2 text-center">
+                            <div class="col md-4 card m-2 text-center bg-success text-white">
                                 <div class="m-auto">
-                                    <span>Total de archivos subidos</span>
-                                    <legend>231</legend>
+                                    <span>Usuarios registrados  </span>
+                                    <i>user</i>
+                                    <legend>23123</legend>
                                 </div>
                             </div>
-                            <div class="col md-4 card m-2 text-center">
+                            <div class="col md-4 card m-2 text-center bg-primary text-white">
                                 <div class="m-auto">
-                                    <span>Archivos subidos esta semana</span>
+                                    <span>Archivos subidos</span>
+                                    <i>files</i>
+                                    <legend>23123</legend>
+                                </div>
+                            </div>
+                            <div class="col md-4 card m-2 text-center bg-success text-white">
+                                <div class="m-auto">
+                                    <span>Tareas completadas</span>
+                                    <i>tasks completed</i>
                                     <legend>0</legend>
                                 </div>
                             </div>
 
-                            <div class="col md-4 card m-2 text-center">
+                            <div class="col md-4 card m-2 text-center bg-warning text-white">
                                 <div class="m-auto">
-                                    <span>Archivos subidos esta semana</span>
-                                    <legend>0</legend>
+                                    <span>Tareas pendientes</span>
+                                    <i>wa  tasks </i>
+                                    <legend>12</legend>
                                 </div>
                             </div>
 
