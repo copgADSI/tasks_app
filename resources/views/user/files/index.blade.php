3bca20b0144bf38ecd7943133c83e832a023f4d9
copgADSI <48689287+copgADSI@users.noreply.github.com>
2023-03-30 16:15:21 -0500

    avances

--- resources/views/user/files/index.blade.php
+++ resources/views/user/files/index.blade.php
@@ -1,88 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/pendrive.css') }}">
-
-    <title>Document</title>
-</head>
-
-<body>
-
+@section('content')
     <div class="container">
-        <div class="d-flex justify-content-center">
-            <div class="drag-area">
-                <div class="icon"><i class="fas fa-cloud-upload-alt"></i></div>
-                <header>Arrastre y suelte el archivo o imagen a subir</header>
-                <span>O</span>
-                <button>Buscar archivo</button>
-                <input type="file" hidden>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Listado de archivos') }}</div>
+
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col md-4 card m-2 text-center">
+                                <div class="m-auto">
+                                    <span>Total de archivos subidos</span>
+                                    <legend>231</legend>
+                                </div>
+                            </div>
+                            <div class="col md-4 card m-2 text-center">
+                                <div class="m-auto">
+                                    <span>Archivos subidos esta semana</span>
+                                    <legend>0</legend>
+                                </div>
+                            </div>
+
+                            <div class="col md-4 card m-2 text-center">
+                                <div class="m-auto">
+                                    <span>Archivos subidos esta semana</span>
+                                    <legend>0</legend>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-
-
-    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"
-        integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous">
-    </script>
-
-</body>
-
-</html>
-
-<script>
-    const dropArea = document.querySelector(".drag-area"),
-        dragText = dropArea.querySelector("header"),
-        button = dropArea.querySelector("button"),
-        input = dropArea.querySelector("input");
-    let file; //this is a global variable and we'll use it inside multiple functions
-    button.onclick = () => {
-        input.click(); //if user click on the button then the input also clicked
-    }
-    input.addEventListener("change", function() {
-        //getting user select file and [0] this means if user select multiple files then we'll select only the first one
-        file = this.files[0];
-        dropArea.classList.add("active");
-        showFile(); //calling function
-    });
-    //If user Drag File Over DropArea
-    dropArea.addEventListener("dragover", (event) => {
-        event.preventDefault(); //preventing from default behaviour
-        dropArea.classList.add("active");
-        dragText.textContent = "Suelte para cargar la imagen";
-    });
-    //If user leave dragged File from DropArea
-    dropArea.addEventListener("dragleave", () => {
-        dropArea.classList.remove("active");
-        dragText.textContent = "Arrastre y suelte para cargar la imagen";
-    });
-    //If user drop File on DropArea
-    dropArea.addEventListener("drop", (event) => {
-        event.preventDefault(); //preventing from default behaviour
-        //getting user select file and [0] this means if user select multiple files then we'll select only the first one
-        file = event.dataTransfer.files[0];
-        showFile(); //calling function
-    });
-
-    function showFile() {
-        let fileType = file.type; //getting selected file type
-        let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
-        if (validExtensions.includes(fileType)) { //if user selected file is an image file
-            let fileReader = new FileReader(); //creating new FileReader object
-            fileReader.onload = () => {
-                let fileURL = fileReader.result; //passing user file source in fileURL variable
-                // UNCOMMENT THIS BELOW LINE. I GOT AN ERROR WHILE UPLOADING THIS POST SO I COMMENTED IT
-                 let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
-                dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
-            }
-            fileReader.readAsDataURL(file);
-        } else {
-            alert("El archivo arrastrado no es de tipo imagen!");
-            dropArea.classList.remove("active");
-            dragText.textContent = "Arraste y suelte para cargar la imagen";
-        }
-    }
-</script>
+@endsection
